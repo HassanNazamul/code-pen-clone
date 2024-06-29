01800077805c9a9c4e770776ddb2026e82dd57ea
Mohammed <hassan.nazamul05@gmail.com>
2024-06-29 04:43:51 -0400

    final project

---
+++ src/components/Navabar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navabar() {
+    return (
+        <div
+            className='main lg:flex md:flex flex-wrap justify-between items-center px-4 bg-[#2f3640] py-4'>
+            <div className="left">
+                <div className="logo font-bold text-2xl text-white text-center">
+                    {/* <img className=' w-44' src="img/codepen.png" alt="" /> */}
+                    <h1>Helllo</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navabar
+
+
