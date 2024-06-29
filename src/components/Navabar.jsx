6375c52cc1b8dbb08f54050e372c70e83c623017
Mohammed <hassan.nazamul05@gmail.com>
2024-06-29 04:44:14 -0400

    Final contribute

--- src/components/Navabar.jsx
+++ src/components/Navabar.jsx
@@ -6,8 +6,7 @@ function Navabar() {
             className='main lg:flex md:flex flex-wrap justify-between items-center px-4 bg-[#2f3640] py-4'>
             <div className="left">
                 <div className="logo font-bold text-2xl text-white text-center">
-                    {/* <img className=' w-44' src="img/codepen.png" alt="" /> */}
-                    <h1>Helllo</h1>
+                    <h1>Codepen-clone</h1>
                 </div>
             </div>
         </div>
